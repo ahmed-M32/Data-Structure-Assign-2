14ae446b9ed160b452d02dd4d6a753cad0d2250c
Medozar <163927813+Medozar@users.noreply.github.com>
2024-05-18 13:53:04 +0300

    Update Heap.cpp

--- Heap.cpp
+++ Heap.cpp
@@ -39,10 +39,40 @@ void Heap::Insert_Item(Item item)
     heap.push_back(item);
 }
 
-void Heap::Heap_sort()
+void Heap::Heap_sort(vector<int>& arr)
 {
-    // to be done
-}
+    int n = arr.size();
+
+    auto heapify = [&](int n, int i) {
+        int largest = i;    
+        int left = 2 * i + 1;   
+        int right = 2 * i + 2;  
+
+        if (left < n && arr[left] > arr[largest]) {
+            largest = left;
+        }
+
+        if (right < n && arr[right] > arr[largest]) {
+            largest = right;
+        }
+
+        if (largest != i) {
+            swap(arr[i], arr[largest]);
+
+            heapify(n, largest);
+        }
+    };
+
+    for (int i = n / 2 - 1; i >= 0; i--) {
+        heapify(n, i);
+    }
+
+    for (int i = n - 1; i > 0; i--) {
+        swap(arr[0], arr[i]);
+
+        heapify(i, 0);
+    }
+}}
 
 void Heap::Max_heapify(int i)
 {
